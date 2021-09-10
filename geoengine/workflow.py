e5849a2cc7c355069d2431dbe2b3f628e48f5a7a
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2021-09-10 14:58:15 +0200

    lint fixes

--- geoengine/workflow.py
+++ geoengine/workflow.py
@@ -434,7 +434,10 @@ class Workflow:
 
         # The dataset is created in the spatial reference system of the workflow result
         if self.get_result_descriptor().spatial_reference != bbox.srs:
-            raise SpatialReferenceMismatchException(self.get_result_descriptor().spatial_reference, bbox.spatial_reference)
+            raise SpatialReferenceMismatchException(
+                self.get_result_descriptor().spatial_reference,
+                bbox.spatial_reference
+            )
 
         session = get_session()
 
