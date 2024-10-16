d06984cadfc68b99aac8aaeaf1594da7147f0af6
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2024-10-16 16:33:05 +0200

    validate inputs for ML

--- geoengine/ml.py
+++ geoengine/ml.py
@@ -6,10 +6,14 @@ from pathlib import Path
 import tempfile
 from typing import Protocol
 from dataclasses import dataclass
-from geoengine_openapi_client.models import MlModelMetadata, MlModel
+from onnx.reference import ReferenceEvaluator
+from onnx import TypeProto, TensorProto, ModelProto
+from onnx.helper import tensor_dtype_to_string
+from geoengine_openapi_client.models import MlModelMetadata, MlModel, RasterDataType
 import geoengine_openapi_client
 from geoengine.auth import get_session
 from geoengine.datasets import UploadId
+from geoengine.error import InputException
 
 
 # pylint: disable=invalid-name
@@ -30,11 +34,18 @@ class MlModelConfig:
     description: str = "My Ml Model Description"
 
 
-def register_ml_model(onnx_model: SerializableModel,
+def register_ml_model(onnx_model: ModelProto,
                       model_config: MlModelConfig,
                       upload_timeout: int = 3600,
                       register_timeout: int = 60):
     '''Uploads an onnx file and registers it as an ml model'''
+
+    validate_model_config(
+        onnx_model,
+        input_type=model_config.metadata.input_type,
+        output_type=model_config.metadata.output_type,
+        num_input_bands=model_config.metadata.num_input_bands,
+    )
 
     session = get_session()
 
@@ -56,3 +67,77 @@ def register_ml_model(onnx_model: SerializableModel,
         model = MlModel(name=model_config.name, upload=str(upload_id), metadata=model_config.metadata,
                         display_name=model_config.display_name, description=model_config.description)
         ml_api.add_ml_model(model, _request_timeout=register_timeout)
+
+
+def validate_model_config(onnx_model: ModelProto, *,
+                          input_type: RasterDataType,
+                          output_type: RasterDataType,
+                          num_input_bands: int):
+    '''Validates the model config. Raises an exception if the model config is invalid'''
+
+    def check_data_type(data_type: TypeProto, expected_type: RasterDataType, prefix: 'str'):
+        if not data_type.tensor_type:
+            raise InputException('Only tensor input types are supported')
+        elem_type = data_type.tensor_type.elem_type
+        if elem_type != raster_type_to_onnx_type(expected_type):
+            elem_type_str = tensor_dtype_to_string(elem_type)
+            raise InputException(f'Model {prefix} type `{elem_type_str}` does not match the '
+                                 f'expected type `{expected_type}`')
+
+    for domain in onnx_model.opset_import:
+        if domain.domain != '':
+            continue
+        if domain.version != 9:
+            raise InputException('Only ONNX models with opset version 9 are supported')
+
+    try:
+        model_ref = ReferenceEvaluator(onnx_model)
+    except NotImplementedError as e:
+        if 'ZipMap' in str(e):
+            raise InputException(
+                'ZipMap is not supported in the model. Consider setting `options={"zipmap": False}`'
+            ) from e
+        else:
+            raise e  # just re-raise
+
+    if len(model_ref.input_types) != 1:
+        raise InputException('Models with multiple inputs are not supported')
+    check_data_type(model_ref.input_types[0], input_type, 'input')
+
+    dims = model_ref.input_types[0].tensor_type.shape.dim
+    if len(dims) != 2:
+        raise InputException('Only 2D input tensors are supported')
+    if not dims[1].dim_value:
+        raise InputException('Dimension 1 of the input tensor must have a length')
+    if dims[1].dim_value != num_input_bands:
+        raise InputException(f'Model input has {dims[1].dim_value} bands, but {num_input_bands} bands are expected')
+
+    if len(model_ref.output_types) < 1:
+        raise InputException('Models with no outputs are not supported')
+    check_data_type(model_ref.output_types[0], output_type, 'output')
+
+
+def raster_type_to_onnx_type(raster_type: RasterDataType) -> TensorProto.DataType:
+    '''Converts a raster data type to an onnx type'''
+    if raster_type == RasterDataType.F32:
+        return TensorProto.FLOAT
+    elif raster_type == RasterDataType.F64:
+        return TensorProto.DOUBLE
+    elif raster_type == RasterDataType.U8:
+        return TensorProto.UINT8
+    elif raster_type == RasterDataType.U16:
+        return TensorProto.UINT16
+    elif raster_type == RasterDataType.U32:
+        return TensorProto.UINT32
+    elif raster_type == RasterDataType.U64:
+        return TensorProto.UINT64
+    elif raster_type == RasterDataType.I8:
+        return TensorProto.INT8
+    elif raster_type == RasterDataType.I16:
+        return TensorProto.INT16
+    elif raster_type == RasterDataType.I32:
+        return TensorProto.INT32
+    elif raster_type == RasterDataType.I64:
+        return TensorProto.INT64
+
+    raise ValueError(f"Unknown raster data type: {raster_type}")
---
+++ tests/test_ml.py
@@ -0,0 +1,154 @@
+'''Tests ML functionality'''
+
+import unittest
+from sklearn.ensemble import RandomForestClassifier
+from skl2onnx import to_onnx
+import numpy as np
+from geoengine_openapi_client.models import MlModelMetadata, RasterDataType
+import geoengine as ge
+from . import UrllibMocker
+
+
+class WorkflowStorageTests(unittest.TestCase):
+    '''Test methods for storing workflows as datasets'''
+
+    def setUp(self) -> None:
+        ge.reset(False)
+
+    def test_uploading_onnx_model(self):
+
+        clf = RandomForestClassifier(random_state=42)
+        x = np.array([[1, 2], [3, 4]], dtype=np.float32)
+        y = np.array([0, 1], dtype=np.int64)
+        clf.fit(x, y)
+
+        onnx_clf = to_onnx(clf, x[:1], options={'zipmap': False}, target_opset=9)
+        onnx_clf2 = to_onnx(clf, x[:1], options={'zipmap': False}, target_opset=12)
+
+        with UrllibMocker() as m:
+            session_id = "c4983c3e-9b53-47ae-bda9-382223bd5081"
+            request_headers = {'Authorization': f'Bearer {session_id}'}
+
+            m.post('http://mock-instance/anonymous', json={
+                "id": session_id,
+                "project": None,
+                "view": None
+            })
+
+            upload_id = "c314ff6d-3e37-41b4-b9b2-3669f13f7369"
+
+            m.post('http://mock-instance/upload', json={
+                "id": upload_id
+            }, request_headers=request_headers)
+
+            m.post('http://mock-instance/ml/models',
+                   expected_request_body={
+                       "description": "A simple decision tree model",
+                       "displayName": "Decision Tree",
+                       "metadata": {
+                           "fileName": "model.onnx",
+                           "inputType": "F32",
+                           "numInputBands": 2,
+                           "outputType": "I64"
+                       },
+                       "name": "foo",
+                       "upload": upload_id
+                   },
+                   request_headers=request_headers)
+
+            ge.initialize("http://mock-instance")
+
+            ge.register_ml_model(
+                onnx_model=onnx_clf,
+                model_config=ge.ml.MlModelConfig(
+                    name="foo",
+                    metadata=MlModelMetadata(
+                        file_name="model.onnx",
+                        input_type=RasterDataType.F32,
+                        num_input_bands=2,
+                        output_type=RasterDataType.I64,
+                    ),
+                    display_name="Decision Tree",
+                    description="A simple decision tree model",
+                )
+            )
+
+            with self.assertRaises(ge.InputException) as exception:
+                ge.register_ml_model(
+                    onnx_model=onnx_clf,
+                    model_config=ge.ml.MlModelConfig(
+                        name="foo",
+                        metadata=MlModelMetadata(
+                            file_name="model.onnx",
+                            input_type=RasterDataType.F32,
+                            num_input_bands=4,
+                            output_type=RasterDataType.I64,
+                        ),
+                        display_name="Decision Tree",
+                        description="A simple decision tree model",
+                    )
+                )
+            self.assertEqual(
+                str(exception.exception),
+                'Model input has 2 bands, but 4 bands are expected'
+            )
+
+            with self.assertRaises(ge.InputException) as exception:
+                ge.register_ml_model(
+                    onnx_model=onnx_clf,
+                    model_config=ge.ml.MlModelConfig(
+                        name="foo",
+                        metadata=MlModelMetadata(
+                            file_name="model.onnx",
+                            input_type=RasterDataType.F64,
+                            num_input_bands=2,
+                            output_type=RasterDataType.I64,
+                        ),
+                        display_name="Decision Tree",
+                        description="A simple decision tree model",
+                    )
+                )
+            self.assertEqual(
+                str(exception.exception),
+                'Model input type `TensorProto.FLOAT` does not match the expected type `RasterDataType.F64`'
+            )
+
+            with self.assertRaises(ge.InputException) as exception:
+                ge.register_ml_model(
+                    onnx_model=onnx_clf,
+                    model_config=ge.ml.MlModelConfig(
+                        name="foo",
+                        metadata=MlModelMetadata(
+                            file_name="model.onnx",
+                            input_type=RasterDataType.F32,
+                            num_input_bands=2,
+                            output_type=RasterDataType.I32,
+                        ),
+                        display_name="Decision Tree",
+                        description="A simple decision tree model",
+                    )
+                )
+            self.assertEqual(
+                str(exception.exception),
+                'Model output type `TensorProto.INT64` does not match the expected type `RasterDataType.I32`'
+            )
+
+            with self.assertRaises(ge.InputException) as exception:
+                ge.register_ml_model(
+                    onnx_model=onnx_clf2,
+                    model_config=ge.ml.MlModelConfig(
+                        name="foo",
+                        metadata=MlModelMetadata(
+                            file_name="model.onnx",
+                            input_type=RasterDataType.F32,
+                            num_input_bands=2,
+                            output_type=RasterDataType.I64,
+                        ),
+                        display_name="Decision Tree",
+                        description="A simple decision tree model",
+                    )
+                )
+            self.assertEqual(
+                str(exception.exception),
+                'Only ONNX models with opset version 9 are supported'
+            )
