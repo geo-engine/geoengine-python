fd068ac54756e72c366b0c0962402fc548c4067a
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2024-12-06 15:19:42 +0100

    update owslib

--- tests/test_wcs.py
+++ tests/test_wcs.py
@@ -427,8 +427,9 @@ class WcsTests(unittest.TestCase):
                 },
                 dims=["band", "y", "x"],
                 attrs={
+                    'AREA_OR_POINT': 'Area',
                     'transform': (45.0, 0.0, -180.0, 0.0, -22.5, 90.0, 0.0, 0.0, 1.0),
-                    'crs': 'EPSG:4326',
+                    # 'crs': "CRS.from_wkt('GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],AXIS[\"Latitude\",NORTH],AXIS[\"Longitude\",EAST],AUTHORITY[\"EPSG\",\"4326\"]]')",
                     'res': (45.0, -22.5),
                     'scale_factor': 1.0,
                     '_FillValue': 0.0,
@@ -449,11 +450,11 @@ class WcsTests(unittest.TestCase):
                     expected.coords),
                 msg=f'{array.coords} \n!=\n {expected.coords}')
 
+            # TODO: Test with np.array_equal(?
             # test attributes
-            self.assertTrue(
-                np.array_equal(
-                    array.attrs, expected.attrs),
-                msg=f'{array.attrs} \n!=\n {expected.attrs}')
+            self.assertEqual(
+                array.attrs, array.attrs | expected.attrs, msg=f'{array.attrs} \n!=\n {expected.attrs}'
+            )
 
 
 if __name__ == '__main__':
