f2a9bd86c0b2a242b91bbaf3b9cda51937c3aace
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2025-02-05 17:13:40 +0100

    use test instance for test_datasets

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -2,7 +2,7 @@
 
 import unittest
 import geoengine as ge
-from . import UrllibMocker
+from tests.ge_test import GeoEngineTestInstance
 
 
 class DatasetsTests(unittest.TestCase):
@@ -15,81 +15,25 @@ class DatasetsTests(unittest.TestCase):
     def test_list_datasets(self):
         """Test `GET /datasets`."""
 
-        with UrllibMocker() as m:
-            m.post('http://mock-instance/anonymous', json={
-                "id": "c4983c3e-9b53-47ae-bda9-382223bd5081",
-                "project": None,
-                "view": None
-            })
+        # TODO: use `enterContext(cm)` instead of `with cm:` in Python 3.11
+        with GeoEngineTestInstance() as ge_instance:
+            ge_instance.wait_for_ready()
 
-            m.get(
-                'http://mock-instance/datasets?filter=foo&order=NameAsc&offset=1&limit=2',
-                request_headers={'Authorization': 'Bearer c4983c3e-9b53-47ae-bda9-382223bd5081'},
-                json=[{
-                    'id': '35383178-4b70-421f-af82-c345c81d2e13',
-                    'name': 'foobar',
-                    'displayName': 'Land Cover',
-                    'description': 'Land Cover',
-                    'tags': [],
-                    'sourceOperator': 'GdalSource',
-                    'resultDescriptor': {'type': 'raster',
-                                         'dataType': 'U8',
-                                         'spatialReference': 'EPSG:4326',
-                                         'bands': [{'name': 'band',
-                                                    'measurement':
-                                                    {'type': 'classification',
-                                                     'measurement': 'Land Cover',
-                                                        'classes': {'0': 'Water Bodies',
-                                                                    '1': 'Evergreen Needleleaf Forests',
-                                                                    '10': 'Grasslands',
-                                                                    '11': 'Permanent Wtlands',
-                                                                    '12': 'Croplands',
-                                                                    '13': 'Urban and Built-Up',
-                                                                    '14': 'Cropland-Natural Vegetation Mosaics',
-                                                                    '15': 'Snow and Ice',
-                                                                    '16': 'Barren or Sparsely Vegetated',
-                                                                    '2': 'Evergreen Broadleaf Forests',
-                                                                    '3': 'Deciduous Needleleaf Forests',
-                                                                    '4': 'Deciduous Broadleleaf Forests',
-                                                                    '5': 'Mixed Forests',
-                                                                    '6': 'Closed Shrublands',
-                                                                    '7': 'Open Shrublands',
-                                                                    '8': 'Woody Savannas',
-                                                                    '9': 'Savannas'}}}],
-                                         'time': None,
-                                         'bbox': {'upperLeftCoordinate': {'x': -180.0, 'y': 90.0},
-                                                  'lowerRightCoordinate': {'x': 180.0, 'y': -90.0}},
-                                         'resolution': {'x': 0.1, 'y': 0.1}},
-                    'symbology': {'type': 'raster',
-                                  'opacity': 1.0,
-                                  'rasterColorizer': {
-                                      'type': 'singleBand',
-                                      'band': 0,
-                                      'bandColorizer': {
-                                          'type': 'linearGradient',
-                                          'breakpoints': [{'value': 0.0, 'color': [0, 0, 255, 255]},
-                                                          {'value': 8.0, 'color': [0, 255, 0, 255]},
-                                                          {'value': 16.0, 'color': [255, 0, 0, 255]}],
-                                          'noDataColor': [0, 0, 0, 0],
-                                          'overColor': [0, 0, 0, 0],
-                                          'underColor': [0, 0, 0, 0]}}}}
-                      ])
-
-            ge.initialize("http://mock-instance")
+            ge.initialize(ge_instance.address())
 
             datasets = ge.list_datasets(
-                offset=1,
-                limit=2,
+                offset=0,
+                limit=10,
                 order=ge.DatasetListOrder.NAME_ASC,
-                name_filter='foo'
+                name_filter="Natural Earth II"
             )
 
-            self.assertEqual(len(datasets), 1)
+            self.assertEqual(len(datasets), 3)
 
             dataset = datasets[0]
 
-            self.assertEqual(dataset.name, 'foobar')
-            self.assertEqual(dataset.display_name, 'Land Cover')
+            self.assertEqual(dataset.name, 'ne2_raster_blue')
+            self.assertEqual(dataset.display_name, 'Natural Earth II – Blue')
             self.assertEqual(dataset.result_descriptor.actual_instance.type, 'raster')
 
 
