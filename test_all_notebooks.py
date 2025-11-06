5422cdf79b5069608ffba586dffb7bc937d73feb
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2025-11-06 14:55:14 +0100

    coverage args as env var

--- test_all_notebooks.py
+++ test_all_notebooks.py
@@ -7,13 +7,15 @@ import shutil
 import subprocess
 import sys
 
+COVERAGE_COMMAND_ENV_VAR = "COVERAGE_COMMAND"
+
 
 def eprint(*args, **kwargs):
     """Print to stderr."""
     print(*args, file=sys.stderr, **kwargs)
 
 
-def run_test_notebook(notebook_path) -> bool:
+def run_test_notebook(notebook_path: str, coverage_args: list[str]) -> bool:
     """Run test_notebook.py for the given notebook."""
 
     pytest_bin = shutil.which("pytest")
@@ -22,7 +24,7 @@ def run_test_notebook(notebook_path) -> bool:
         raise RuntimeError("Python 3 not found")
 
     result = subprocess.run(
-        [pytest_bin, "--ignore=test", "--cov", "--cov-append", "test_notebook.py"],
+        [pytest_bin, "--ignore=test", *coverage_args, "test_notebook.py"],
         env={
             **os.environ,
             "INPUT_FILE": notebook_path,
@@ -40,6 +42,14 @@ def run_test_notebook(notebook_path) -> bool:
         return False
 
 
+def parse_coverage_command() -> list[str]:
+    """Get coverage command from environment variable."""
+    coverage_cmd = os.getenv(COVERAGE_COMMAND_ENV_VAR)
+    if not coverage_cmd:
+        return []
+    return [*coverage_cmd.split(), "--cov-append"]
+
+
 def main() -> int:
     """Run all Jupyter Notebooks and check for errors."""
 
@@ -49,13 +59,19 @@ def main() -> int:
         eprint(f"The folder {example_folder} does not exist.")
         return -1
 
+    coverage_args = parse_coverage_command()
+    if coverage_args:
+        eprint(f"Using coverage args: {' '.join(coverage_args)}")
+    else:
+        eprint(f"No coverage args in env {COVERAGE_COMMAND_ENV_VAR} provided.")
+
     for root, _dirs, files in os.walk(example_folder):
         for file in files:
             if not file.endswith(".ipynb"):
                 eprint(f"Skipping non-notebook file {file}")
                 continue
             notebook_path = os.path.join(root, file)
-            if not run_test_notebook(notebook_path):
+            if not run_test_notebook(notebook_path, coverage_args):
                 return -1
 
         break  # skip subdirectories
